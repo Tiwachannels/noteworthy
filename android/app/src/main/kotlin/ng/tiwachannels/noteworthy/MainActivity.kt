2fe6adabdbeb2baf573bf8bff9be4ecf2dd66837
derinto10 <derinajibade@gmail.com>
2023-08-13 12:54:31 +0100

    my initial commit

---
+++ android/app/src/main/kotlin/ng/tiwachannels/noteworthy/MainActivity.kt
@@ -0,0 +1,6 @@
+package ng.tiwachannels.noteworthy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/login_view.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/material.dart';
+
+
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  
+  
+  
+
+}
